46fd50c4617ad3a5bd68da35d490214fc774c7b6
andre.matos <andre.matos@sisloc.com.br>
2024-02-11 00:36:27 -0300

    #01 - teste unitario manual

---
+++ index.js
@@ -0,0 +1,36 @@
+const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras
+
+const calculaDescontos = (salario, descontos) => salario - descontos
+
+const verifiqueSe = (valor) => {
+  const assercoes = {
+    ehexatamenteIgualA(esperado) {
+      if (valor !== esperado) {
+        // eslint-disable-next-line no-throw-literal
+        throw {}
+      }
+    },
+  }
+  return assercoes
+}
+
+const teste = (titulo, funcaoDeTeste) => {
+  try {
+    funcaoDeTeste()
+    console.log(`${titulo} Passou!`)
+  } catch (error) {
+    console.log(`${titulo} não Passou!`)
+  }
+}
+
+teste('somaHorasExtras', () => {
+  const esperado = 2500
+  const retornado = somaHorasExtras(2000, 500)
+  verifiqueSe(retornado).ehexatamenteIgualA(esperado)
+})
+
+teste('calculaDesconto', () => {
+  const esperado = 2200
+  const retornado = calculaDescontos(2500, 300)
+  verifiqueSe(retornado).ehexatamenteIgualA(esperado)
+})
